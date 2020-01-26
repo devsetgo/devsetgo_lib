44d65c1c058183c1af79aefae11cedb95fec729b
mike ryan <mikeryan56@gmail.com>
2020-01-26 16:23:39 -0500

    adding copy of index and readme

--- scripts/mkdocs.sh
+++ scripts/mkdocs.sh
@@ -6,4 +6,5 @@ set -x
 mkdocs build
 
 # Copy Contribute to Github Contributing
+cp ~/devsetgo_lib/docs/index.md ~/devsetgo_lib/README.md
 cp ~/devsetgo_lib/docs/contribute.md ~/devsetgo_lib/CONTRIBUTING.md
