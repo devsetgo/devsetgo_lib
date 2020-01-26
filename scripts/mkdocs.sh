e2b603b8fcdf78bc3f4110ccefb7bb27e91dd2d6
mike ryan <mikeryan56@gmail.com>
2020-01-26 16:35:39 -0500

    adding publish to github pages

--- scripts/mkdocs.sh
+++ scripts/mkdocs.sh
@@ -8,3 +8,5 @@ mkdocs build
 # Copy Contribute to Github Contributing
 cp ~/devsetgo_lib/docs/index.md ~/devsetgo_lib/README.md
 cp ~/devsetgo_lib/docs/contribute.md ~/devsetgo_lib/CONTRIBUTING.md
+
+mkdocs gh-deploy
