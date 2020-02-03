d7d0902f33c62dd588cf38c5df5d5765bcad585e
mike ryan <mikeryan56@gmail.com>
2020-02-02 19:40:59 -0500

    adding text example

---
+++ examples/text_examples.py
@@ -0,0 +1,35 @@
+from devsetgo_lib.file_functions import save_text, open_text
+
+example_text = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
+ """
+
+
+def save_some_data(example_text: str):
+    # function requires file_name and data as a string to be sent.
+    # see documentation for additonal information
+    save_text(file_name="your-file-name.txt", data=example_text)
+
+
+def open_some_data(the_file_name: str) -> dict:
+    # function requires file_name and a string will be returned
+    # see documentation for additonal information
+    result: dict = open_text(file_name=the_file_name)
+    return result
+
+
+if __name__ == "__main__":
+    save_some_data(example_text)
+    opened_file: dict = open_some_data("your-file-name.txt")
+    print(opened_file)
