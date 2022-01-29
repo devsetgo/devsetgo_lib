42395b802387071fca6d3d0cb47823d9ea323668
mike <mikeryan56@gmail.com>
2022-01-28 23:10:13 -0500

    clean up

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -7,14 +7,13 @@ set -x
 
 #run pre-commit
 pre-commit run -a
-
 # bash scripts/test.sh --cov-report=html ${@}
-python3 -m pytest
+pytest
+# python3 -m pytest
 # python3 -m pytest -v -s
 # modify path for
 sed -i "s/<source>\/home\/mike\/devsetgo_lib<\/source>/<source>\/github\/workspace<\/source>/g" ~/devsetgo_lib/coverage.xml
 # create coverage-badge
 coverage-badge -o ../coverage.svg -f
-
 # generate flake8 report
-# flake8 --tee . > flake8_report/report.txt
+flake8 --tee . > flake8_report/report.txt
