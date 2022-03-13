32837f2be6eff006e2ebeb14089a8087097fb9ee
mike <mikeryan56@gmail.com>
2022-03-13 02:17:40 +0000

    document updates

--- scripts/mkdocs.sh
+++ scripts/mkdocs.sh
@@ -6,7 +6,7 @@ set -x
 mkdocs build
 
 # Copy Contribute to Github Contributing
-cp ~/devsetgo_lib/docs/index.md ~/devsetgo_lib/README.md
-cp ~/devsetgo_lib/docs/contribute.md ~/devsetgo_lib/CONTRIBUTING.md
+cp /workspaces/devsetgo_lib/docs/index.md /workspaces/devsetgo_lib/README.md
+cp /workspaces/devsetgo_lib/docs/contribute.md /workspaces/devsetgo_lib/CONTRIBUTING.md
 
 mkdocs gh-deploy
