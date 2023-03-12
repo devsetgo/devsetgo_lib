8ea03d1fb83f47047777e1166237cccf426f89b2
devsetgo <mikeryan56@gmail.com>
2023-03-12 03:38:00 +0000

    code improvements and tests

--- dsg_lib/folder_functions.py
+++ dsg_lib/folder_functions.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
+import re
 from datetime import datetime
 from pathlib import Path
-import re
 
 log_format = "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
 logging.basicConfig(format=log_format, level=logging.INFO)
@@ -14,33 +14,64 @@ directory_path = Path.cwd().joinpath(file_directory)
 
 
 def last_data_files_changed(directory_path):
+    """
+    Get the last modified file in a directory and return its modification time and path.
+
+    Args:
+        directory_path (pathlib.Path): The directory to search for the last modified file.
+
+    Returns:
+        Tuple[datetime.datetime, pathlib.Path]: A tuple containing the modification time and path of the last modified file,
+        or (None, None) if there was an error.
+
+    """
     try:
+        # Use a generator expression to find the last modified file in the directory
         time, file_path = max((f.stat().st_mtime, f) for f in directory_path.iterdir())
+
+        # Convert the modification time to a datetime object
         time_stamp = datetime.fromtimestamp(time)
 
-        logging.info(f"directory checked for last change: {file_directory}")
+        # Log a message to indicate that the directory was checked for the last modified file
+        logging.info(f"Directory checked for last change: {directory_path}")
+
+        # Return the modification time and path of the last modified file
         return time_stamp, file_path
-    except Exception as e:
-        logging.error(e)
+
+    except Exception as err:
+        # Log an error message if an exception occurs, and return a default value to indicate an error
+        logging.error(err)
+        return None, None
 
 
 def get_directory_list(file_directory):
-    """getting a list of directories"""
-    direct_list = []
+    """Get a list of directories in the specified directory.
+
+    Args:
+        file_directory (str): The directory to search for directories.
+
+    Returns:
+        A list of directories in the specified directory.
+
+    Raises:
+        FileNotFoundError: If the specified directory does not exist.
+    """
+    # Create a Path object for the specified directory
     file_path = Path.cwd().joinpath(file_directory)
+
     try:
-        # loop through directory
-        for x in file_path.iterdir():
-            # check if it is a directory
-            if x.is_dir():
-                # add to list
-                direct_list.append(x)
-        # return list of items in directory
-        logging.info(f"getting a list of directories: {file_directory}")
+        # Use a list comprehension to create a list of directories in the specified directory
+        direct_list = [x for x in file_path.iterdir() if x.is_dir()]
+
+        # Log a message indicating that the list of directories was retrieved
+        logging.info(f"Retrieved list of directories: {file_directory}")
+
+        # Return the list of directories
         return direct_list
 
-    except FileNotFoundError as e:
-        logging.error(e)
+    except FileNotFoundError as err:
+        # Log an error message if the specified directory does not exist
+        logging.error(err)
 
 
 def make_folder(file_directory):
@@ -79,9 +110,38 @@ def make_folder(file_directory):
 
 
 def remove_folder(file_directory):
-    """making a folder in a specific directory"""
+    """Remove a folder from the specified directory.
+
+    Args:
+        file_directory (str): The directory containing the folder to be removed.
+
+    Returns:
+        None.
+
+    Raises:
+        FileNotFoundError: If the specified directory does not exist.
+        OSError: If the specified folder could not be removed.
+    """
     try:
-        Path.rmdir(file_directory)
-        logging.info(f"direct removed: at {file_directory}")
-    except OSError as e:
-        logging.error(e)
+        # Create a Path object for the specified directory
+        path = Path(file_directory)
+
+        # Use the rmdir method of the Path object to remove the folder
+        path.rmdir()
+
+        # Log a message indicating that the folder was removed
+        logging.info(f"Folder removed: {file_directory}")
+
+    except FileNotFoundError as err:
+        # Log an error message if the specified directory does not exist
+        logging.error(err)
+
+        # Raise the FileNotFoundError exception to be handled by the calling code
+        raise
+
+    except OSError as err:
+        # Log an error message if the folder could not be removed
+        logging.error(err)
+
+        # Raise the OSError exception to be handled by the calling code
+        raise
