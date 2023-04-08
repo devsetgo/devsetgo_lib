9c3e3537205b38d299875f104356a448e5781f2a
devsetgo <mikeryan56@gmail.com>
2023-04-08 19:42:43 +0000

    optimizing the regex pattern

--- dsg_lib/patterns.py
+++ dsg_lib/patterns.py
@@ -1,34 +1,70 @@
-# -*- coding: utf-8 -*-
 import logging
 import re
 
+# Set up logging configuration
 log_format = {
-    "asctime": "%(asctime)s [UTC%(asctime:z)]",
+    "asctime": "%(asctime)s [UTC%(z)s]",
     "name": "%(name)s",
     "levelname": "%(levelname)s",
     "message": "%(message)s",
 }
-
 logging.basicConfig(format=log_format, level=logging.INFO)
 
 
 def pattern_between_two_char(
     text_string: str, left_characters: str, right_characters: str
 ) -> dict:
+    """
+    This function searches for any substring that occurs between two specified characters in a given text string.
+    It returns a dictionary containing the found substring(s), the number of matches found, and the regex pattern
+    used to search for these matches.
+
+    Parameters:
+    -----------
+        text_string : str
+            Text string to be searched for matching substrings.
+        left_characters : str
+            The opening character that identifies the start of the target substring.
+        right_characters : str
+            The closing character that identifies the end of the target substring.
+
+    Returns:
+    --------
+        Dictionary with the following keys:
+        - 'found': A list of all matching substrings found.
+        - 'matched_found': The number of matched substrings found (can be zero).
+        - 'pattern_parameters': A dictionary containing the regex pattern used and the original input parameters.
+
+    Example:
+    ---------
+    # Finding all strings between single quotes in the given string
+    >>> pattern_between_two_char("This is a 'test' string with 'multiple' quotes.", "'", "'")
+    {'found': ['test', 'multiple'], 'matched_found': 2, 'pattern_parameters':
+     {'left_character': "\'", 'right_character': "\'", 'regex_pattern': "'(.+?)'?", 'text_string':
+     'This is a \'test\' string with \'multiple\' quotes.'}}
+    """
+
     try:
-        if left_characters.isprintable() is False:
-            raise ValueError("The left character is not printable and cannot be used.")
+        # Check if left character is an alphanumeric
+        if not left_characters.isalnum():
+            raise ValueError("The left character is not alphanumeric and cannot be used.")
 
-        if right_characters.isprintable() is False:
-            raise ValueError("The right character is not printable and cannot be used.")
-
+        # Check if right character is an alphanumeric
+        if not right_characters.isalnum():
+            raise ValueError("The right character is not alphanumeric and cannot be used.")
+        
+        # Escape input strings to safely use it in regex pattern
         esc_text = re.escape(text_string)
         esc_left_char = re.escape(left_characters)
         esc_right_char = re.escape(right_characters)
 
-        pattern = f"{esc_left_char}(.+?){esc_right_char}+?"
+        # Create a regex pattern that matches all substrings between target characters
+        pattern = f"{esc_left_char}(.+?){esc_right_char}?"
 
+        # Search for all patterns and store them in pattern_list variable
         pattern_list = re.findall(pattern, esc_text)
+
+        # Create a dictionary to store match details
         results: dict = {
             "found": pattern_list,
             "matched_found": len(pattern_list),
@@ -39,11 +75,18 @@ def pattern_between_two_char(
                 "text_string": esc_text,
             },
         }
+        
+        # Log matched pattern(s) found using 'debug' log level
+        if len(pattern_list) > 0:
+            logging.debug(f"Matched pattern(s): {pattern_list}")
+
+        # Log successful function execution using 'info' log level
+        logging.info(f"Successfully executed 'pattern_between_two_char' function")
 
         return results
 
     except ValueError as e:
-        # capture exception and return results
+        # capture exception and return error in case of invalid input parameters
         results: dict = {
             "Error": str(e),
             "matched_found": 0,
@@ -54,6 +97,6 @@ def pattern_between_two_char(
                 "text_string": text_string,
             },
         }
-        # logging of regex error
-        logging.critical(results)
+        # logging of regex error using 'critical' log level
+        logging.critical(f"Failed to generate regex pattern with error: {e}")
         return results
