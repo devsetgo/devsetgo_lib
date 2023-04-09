2355e20e7767460a5724792e6604b0487d47c3a9
devsetgo <mikeryan56@gmail.com>
2023-04-09 01:13:44 +0000

    improvements and tests

--- dsg_lib/patterns.py
+++ dsg_lib/patterns.py
@@ -1,65 +1,55 @@
+# -*- coding: utf-8 -*-
 import logging
 import re
-
-# Set up logging configuration
-log_format = {
-    "asctime": "%(asctime)s [UTC%(z)s]",
-    "name": "%(name)s",
-    "levelname": "%(levelname)s",
-    "message": "%(message)s",
-}
-logging.basicConfig(format=log_format, level=logging.INFO)
 
 
 def pattern_between_two_char(
     text_string: str, left_characters: str, right_characters: str
 ) -> dict:
     """
-    This function searches for any substring that occurs between two specified characters in a given text string.
-    It returns a dictionary containing the found substring(s), the number of matches found, and the regex pattern
-    used to search for these matches.
+    Searches for all patterns between two characters (left and right) in a given string using regular expressions.
 
-    Parameters:
-    -----------
-        text_string : str
-            Text string to be searched for matching substrings.
-        left_characters : str
-            The opening character that identifies the start of the target substring.
-        right_characters : str
-            The closing character that identifies the end of the target substring.
+    Args:
+        text_string: The string in which to search for patterns
+        left_characters: The character(s) that appear(s) immediately to the left of the desired pattern
+        right_characters: The character(s) that appear(s) immediately to the right of the desired pattern
 
     Returns:
-    --------
-        Dictionary with the following keys:
-        - 'found': A list of all matching substrings found.
-        - 'matched_found': The number of matched substrings found (can be zero).
-        - 'pattern_parameters': A dictionary containing the regex pattern used and the original input parameters.
+        A dictionary with the following keys:
+            - "found": a list of strings containing all patterns found
+            - "matched_found": the number of patterns found
+            - "pattern_parameters": a dictionary with the following keys:
+                - "left_character": the escaped left character string used to build the regex pattern
+                - "right_character": the escaped right character string used to build the regex pattern
+                - "regex_pattern": the final regex pattern used for searching
+                - "text_string": the escaped input string used for searching
 
-    Example:
-    ---------
-    # Finding all strings between single quotes in the given string
-    >>> pattern_between_two_char("This is a 'test' string with 'multiple' quotes.", "'", "'")
-    {'found': ['test', 'multiple'], 'matched_found': 2, 'pattern_parameters':
-     {'left_character': "\'", 'right_character': "\'", 'regex_pattern': "'(.+?)'?", 'text_string':
-     'This is a \'test\' string with \'multiple\' quotes.'}}
+        If an error occurs during execution, a dictionary with the following keys is returned instead:
+            - "Error": a string containing the error message
+            - "matched_found": 0
+            - "pattern_parameters": a dictionary with the following keys:
+                - "left_character": the original left character string passed as input
+                - "right_character": the original right character string passed as input
+                - "regex_pattern": None
+                - "text_string": the original input string passed as input
     """
 
-    try:
-        # Check if left character is an alphanumeric
-        if not left_characters.isalnum():
-            raise ValueError("The left character is not alphanumeric and cannot be used.")
+    if not left_characters or not right_characters:
+        raise ValueError(
+            f"Left '{left_characters}' and/or Right '{right_characters}' characters must not be None or empty"
+        )
 
-        # Check if right character is an alphanumeric
-        if not right_characters.isalnum():
-            raise ValueError("The right character is not alphanumeric and cannot be used.")
-        
-        # Escape input strings to safely use it in regex pattern
+    try:
+        # Escape input strings to safely use them in regex pattern
         esc_text = re.escape(text_string)
         esc_left_char = re.escape(left_characters)
         esc_right_char = re.escape(right_characters)
 
         # Create a regex pattern that matches all substrings between target characters
-        pattern = f"{esc_left_char}(.+?){esc_right_char}?"
+        pattern = f"{esc_left_char}(.+?){esc_right_char}"
+
+        # Replace \w with . to match any printable UTF-8 character
+        pattern = pattern.replace(r"\w", r".")
 
         # Search for all patterns and store them in pattern_list variable
         pattern_list = re.findall(pattern, esc_text)
@@ -75,20 +65,19 @@ def pattern_between_two_char(
                 "text_string": esc_text,
             },
         }
-        
+
         # Log matched pattern(s) found using 'debug' log level
         if len(pattern_list) > 0:
             logging.debug(f"Matched pattern(s): {pattern_list}")
 
         # Log successful function execution using 'info' log level
-        logging.info(f"Successfully executed 'pattern_between_two_char' function")
-
+        logging.info("Successfully executed 'pattern_between_two_char' function")
         return results
 
-    except ValueError as e:
+    except ValueError as e:  # pragma: no cover
         # capture exception and return error in case of invalid input parameters
         results: dict = {
-            "Error": str(e),
+            "error": str(e),
             "matched_found": 0,
             "pattern_parameters": {
                 "left_character": left_characters,
