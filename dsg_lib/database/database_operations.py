0de844649c85023949b01a568f51035b15788695
devsetgo <mikeryan56@gmail.com>
2023-12-04 01:29:43 +0000

    adding net functions

--- dsg_lib/database/database_operations.py
+++ dsg_lib/database/database_operations.py
@@ -23,22 +23,18 @@ This module is designed to be used in an asynchronous context and requires Pytho
 """
 
 import time  # Importing time module to work with times
-
 # Importing Dict and List from typing for type hinting
 from typing import Dict
 
 # Importing MetaData and func from sqlalchemy for database operations
 from sqlalchemy import func
-
 # Importing specific exceptions from sqlalchemy for error handling
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
-
 # Importing select from sqlalchemy for making select queries
 from sqlalchemy.future import select
 
 # import logging as logger  # Importing logging module for logging
 from ..logger import logger
-
 # Importing AsyncDatabase class from local module async_database
 from .async_database import AsyncDatabase
 
@@ -67,25 +63,7 @@ def handle_exceptions(ex):
 
 
 class DatabaseOperations:
-    """
-        The DatabaseOperations class provides methods for executing various types of database operations asynchronously using SQLAlchemy.
 
-    This class is initialized with an instance of the AsyncDatabase class, which is used for performing the actual database operations.
-
-    The class provides the following methods:
-
-    __init__: Initializes a new instance of the DatabaseOperations class.
-    count_query: Executes a count query and returns the result.
-    read_query: Executes a fetch query and returns the result.
-    read_multi_query: Executes multiple fetch queries and returns the results.
-    create_one: Adds a single record to the database.
-    create_many: Adds multiple records to the database.
-    update_one: Updates a single record in the database.
-    delete_one: Deletes a single record from the database.
-    Each method is designed to handle exceptions and log errors and information messages using the logging module.
-
-    This class is designed to be used in an asynchronous context and requires Python 3.7+.
-    """
 
     def __init__(self, async_db: AsyncDatabase):
         """
@@ -101,165 +79,159 @@ class DatabaseOperations:
         self.async_db = async_db
         logger.info("DatabaseOperations instance initialized successfully")
 
+
+    async def get_columns_details(self, table):
+        logger.debug(f"Starting get_columns_config operation for table: {table.__name__}")
+        try:
+            logger.debug(f"Getting columns for table: {table.__name__}")
+            columns = {
+                c.name: {
+                    "type": str(c.type),
+                    "nullable": c.nullable,
+                    "primary_key": c.primary_key,
+                    "unique": c.unique,
+                    "autoincrement": c.autoincrement,
+                    "default": str(c.default.arg) if c.default is not None and not callable(c.default.arg) else None,
+                }
+                for c in table.__table__.columns
+            }
+            logger.info(f"Columns retrieved successfully: {columns}")
+            return columns
+        except Exception as ex:
+            logger.error(f"Exception occurred: {ex}")
+            return handle_exceptions(ex)
+
+
+    async def get_primary_keys(self, table):
+
+        logger.debug(f"Starting get_primary_keys operation for table: {table.__name__}")
+        try:
+
+            logger.debug(f"Getting primary keys for table: {table.__name__}")
+            primary_keys = table.__table__.primary_key.columns.keys()
+            logger.info(f"Primary keys retrieved successfully: {primary_keys}")
+            return primary_keys
+
+        except Exception as ex:
+            logger.error(f"Exception occurred: {ex}")
+            return handle_exceptions(ex)
+
+
+    async def get_one_record(self, query):
+
+        logger.debug(
+            f"Starting get_one_record operation for {query}"
+        )
+        try:
+            async with self.async_db.get_db_session() as session:
+
+                logger.debug(f"Getting record with query: {query}")
+                result = await session.execute(query)
+                record = result.scalar_one()
+                logger.info(f"Record retrieved successfully: {record}")
+                return record
+
+        except Exception as ex:
+            logger.error(f"Exception occurred: {ex}")
+            return handle_exceptions(ex)
+
+
     async def create_one(self, record):
-        """
-        Adds a single record to the database.
 
-        This method takes a dictionary representing a record, adds it to the database session, and commits the session. If the operation is successful, it returns the inserted record. If an error occurs during the operation, it raises a DatabaseOperationException.
-
-        Parameters:
-        record (dict): The record to add to the database, represented as a dictionary where the keys are the column names and the values are the corresponding column values.
-
-        Returns:
-        dict: The record that was added to the database.
-
-        Raises:
-        DatabaseOperationException: If an error occurs during the database operation.
-        """
         logger.debug("Starting create_one operation")
         try:
             async with self.async_db.get_db_session() as session:
-                # Add the record to the session and commit
+
                 logger.debug(f"Adding record to session: {record.__dict__}")
                 session.add(record)
                 await session.commit()
-                logger.info(f"Record added successfully: {record.id}")
+                logger.info(f"Record added successfully: {record}")
                 return record
-        # Catch any exception that occurs during the operation
+
         except Exception as ex:
-            # Call the handle_exceptions function to handle the exception.
-            # This function checks the type of the exception and returns an appropriate error dictionary.
-            # This way, we can handle multiple types of exceptions in a consistent manner across different methods.
+            logger.error(f"Exception occurred: {ex}")
             return handle_exceptions(ex)
 
+
     async def create_many(self, records):
-        """
-        Adds multiple records to the database.
 
-        This method takes a list of dictionaries representing records, adds them all to the database session, and commits the session. If the operation is successful, it returns the inserted records. If an error occurs during the operation, it logs the error and returns a dictionary containing the error details.
-
-        Parameters:
-        records (list[dict]): The records to add to the database, each represented as a dictionary where the keys are the column names and the values are the corresponding column values.
-
-        Returns:
-        list[dict] or dict: The records that were added to the database if the operation is successful, otherwise a dictionary containing the error details.
-        """
         logger.debug("Starting create_many operation")
         try:
-            t0 = time.time()  # Record the start time of the operation
+            t0 = time.time()
             async with self.async_db.get_db_session() as session:
-                # Add all the records to the session and commit
+
                 logger.debug(f"Adding {len(records)} records to session")
-                session.add_all(records)  # Add all records to the session
-                await session.commit()  # Commit the session to save the records to the database
-                # Convert each record to a dictionary and log it
+                session.add_all(records)
+                await session.commit()
                 records_data = [record.__dict__ for record in records]
                 logger.debug(f"Records added to session: {records_data}")
 
-                num_records = len(records)  # Get the number of records added
-                t1 = time.time() - t0  # Calculate the time taken for the operation
+                num_records = len(records)
+                t1 = time.time() - t0
                 logger.info(
                     f"Record operations were successful. {num_records} records were created in {t1:.4f} seconds."
                 )
-                return records  # Return the list of records added
-        # Catch any exception that occurs during the operation
+                return records
+
         except Exception as ex:
-            # Call the handle_exceptions function to handle the exception.
-            # This function checks the type of the exception and returns an appropriate error dictionary.
-            # This way, we can handle multiple types of exceptions in a consistent manner across different methods.
+            logger.error(f"Exception occurred: {ex}")
             return handle_exceptions(ex)
 
+
     async def count_query(self, query):
-        """
-        Executes a count query and returns the result.
 
-        This method takes a SQLAlchemy query as input, executes it against the database to count the number of matching records, and returns the count. If an error occurs during the operation, it logs the error and returns a dictionary containing the error details.
-
-        Parameters:
-        query (sqlalchemy.sql.selectable.Select): The SQLAlchemy query to execute.
-
-        Returns:
-        int or dict: The count of matching records if the operation is successful, otherwise a dictionary containing the error details.
-        """
         logger.debug("Starting count_query operation")
         try:
             async with self.async_db.get_db_session() as session:
-                # Execute the count query
+
                 logger.debug(f"Executing count query: {query}")
                 result = await session.execute(select(func.count()).select_from(query))
                 count = result.scalar()
                 logger.info(f"Count query executed successfully. Result: {count}")
                 return count
-        # Catch any exception that occurs during the operation
+
         except Exception as ex:
-            # Call the handle_exceptions function to handle the exception.
-            # This function checks the type of the exception and returns an appropriate error dictionary.
-            # This way, we can handle multiple types of exceptions in a consistent manner across different methods.
+            logger.error(f"Exception occurred: {ex}")
             return handle_exceptions(ex)
 
+
     async def read_query(self, query, limit=500, offset=0):
-        """
-        Executes a fetch query and returns the result.
 
-        This method takes a SQLAlchemy query, a limit, and an offset as input, executes the query against the database to fetch the matching records, and returns the records. It supports pagination through the limit and offset parameters. If an error occurs during the operation, it logs the error and returns a dictionary containing the error details.
-
-        Parameters:
-        query (sqlalchemy.sql.selectable.Select): The SQLAlchemy query to execute.
-        limit (int, optional): The maximum number of records to return. Defaults to 500.
-        offset (int, optional): The number of records to skip before starting to fetch the records. Defaults to 0.
-
-        Returns:
-        list or dict: The list of matching records if the operation is successful, otherwise a dictionary containing the error details.
-        """
         logger.debug("Starting read_query operation")
         try:
             async with self.async_db.get_db_session() as session:
-                # Execute the fetch query with the given limit and offset
+
                 logger.debug(
                     f"Executing fetch query: {query} with limit: {limit} and offset: {offset}"
                 )
                 result = await session.execute(query.limit(limit).offset(offset))
                 records = result.scalars().all()
 
-                # Convert each record to a dictionary and log it
+
                 records_data = [record.__dict__ for record in records]
                 logger.info(
                     f"Fetch query executed successfully. Records: {records_data}"
                 )
 
                 return records
-        # Catch any exception that occurs during the operation
+
         except Exception as ex:
-            # Call the handle_exceptions function to handle the exception.
-            # This function checks the type of the exception and returns an appropriate error dictionary.
-            # This way, we can handle multiple types of exceptions in a consistent manner across different methods.
+            logger.error(f"Exception occurred: {ex}")
             return handle_exceptions(ex)
 
+
     async def read_multi_query(self, queries: Dict[str, str], limit=500, offset=0):
-        """
-        Executes multiple fetch queries and returns the results.
 
-        This method takes a dictionary of SQL queries, executes each query against the database to fetch the matching records, and returns a dictionary of results. Each key in the result dictionary corresponds to a query name, and the value is a list of matching records for that query. The method also supports pagination through the limit and offset parameters.
-
-        Parameters:
-        queries (Dict[str, str]): A dictionary where the key is the query name and the value is the SQL query to execute.
-        limit (int, optional): The maximum number of records to return for each query. Defaults to 500.
-        offset (int, optional): The number of records to skip before starting to fetch the records for each query. Defaults to 0.
-
-        Returns:
-        Dict[str, list]: A dictionary where the key is the query name and the value is a list of matching records.
-        """
         logger.debug("Starting read_multi_query operation")
         try:
             results = {}
             async with self.async_db.get_db_session() as session:
-                # Execute each fetch query with the given limit and offset
+
                 for query_name, query in queries.items():
                     logger.debug(f"Executing fetch query: {query}")
                     result = await session.execute(query.limit(limit).offset(offset))
                     data = result.scalars().all()
 
-                    # Convert each record to a dictionary and log it
                     data_dicts = [record.__dict__ for record in data]
                     logger.debug(f"Fetch result for query '{query_name}': {data_dicts}")
                     logger.info(
@@ -268,27 +240,13 @@ class DatabaseOperations:
 
                     results[query_name] = data
             return results
-        # Catch any exception that occurs during the operation
+
         except Exception as ex:
-            # Call the handle_exceptions function to handle the exception.
-            # This function checks the type of the exception and returns an appropriate error dictionary.
-            # This way, we can handle multiple types of exceptions in a consistent manner across different methods.
+            logger.error(f"Exception occurred: {ex}")
             return handle_exceptions(ex)
 
+
     async def update_one(self, table, record_id: str, new_values: dict):
-        """
-        Updates a single record in the database.
-
-        This method takes a table, a record ID, and a dictionary of new values. It fetches the record from the database using the provided ID, updates the record with the new values, and commits the session. If the operation is successful, it returns the updated record. If an error occurs during the operation, it logs the error and returns a dictionary containing the error details.
-
-        Parameters:
-        table (Table): The table in the database where the record is located.
-        record_id (str): The ID of the record to update.
-        new_values (dict): The new values to update the record with, represented as a dictionary where the keys are the column names and the values are the new column values.
-
-        Returns:
-        dict or dict: The record that was updated in the database if the operation is successful, otherwise a dictionary containing the error details.
-        """
         non_updatable_fields = ["id", "date_created"]
 
         logger.debug(
@@ -297,7 +255,7 @@ class DatabaseOperations:
 
         try:
             async with self.async_db.get_db_session() as session:
-                # Fetch the record to be updated
+
                 logger.debug(f"Fetching record with id: {record_id}")
                 record = await session.get(table, record_id)
                 if not record:
@@ -307,7 +265,6 @@ class DatabaseOperations:
                         "details": f"No record found with id {record_id}",
                     }
 
-                # Update the record with new values
                 logger.debug(f"Updating record with new values: {new_values}")
                 for key, value in new_values.items():
                     if key not in non_updatable_fields:
@@ -315,26 +272,14 @@ class DatabaseOperations:
                 await session.commit()
                 logger.info(f"Record updated successfully: {record.id}")
                 return record
-        # Catch any exception that occurs during the operation
+
         except Exception as ex:
-            # Call the handle_exceptions function to handle the exception.
-            # This function checks the type of the exception and returns an appropriate error dictionary.
-            # This way, we can handle multiple types of exceptions in a consistent manner across different methods.
+            logger.error(f"Exception occurred: {ex}")
             return handle_exceptions(ex)
 
+
     async def delete_one(self, table, record_id: str):
-        """
-        Deletes a single record from the database.
 
-        This method takes a table and a record ID. It fetches the record from the database using the provided ID, deletes the record from the session, and commits the session. If the operation is successful, it returns a success message. If an error occurs during the operation, it logs the error and returns a dictionary containing the error details.
-
-        Parameters:
-        table (Table): The table in the database where the record is located.
-        record_id (str): The ID of the record to delete.
-
-        Returns:
-        dict: A success message if the operation is successful, otherwise a dictionary containing the error details.
-        """
         logger.debug(
             f"Starting delete_one operation for record_id: {record_id} in table: {table.__name__}"
         )
@@ -367,9 +312,6 @@ class DatabaseOperations:
                 logger.info(f"Record deleted successfully: {record_id}")
                 return {"success": "Record deleted successfully"}
 
-        # Catch any exception that occurs during the operation
         except Exception as ex:
-            # Call the handle_exceptions function to handle the exception.
-            # This function checks the type of the exception and returns an appropriate error dictionary.
-            # This way, we can handle multiple types of exceptions in a consistent manner across different methods.
+            logger.error(f"Exception occurred: {ex}")
             return handle_exceptions(ex)
