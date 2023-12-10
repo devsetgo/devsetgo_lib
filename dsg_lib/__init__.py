74afa9c225856631ffe5c16074f7512c5ea65204
devsetgo <mikeryan56@gmail.com>
2023-12-10 04:44:59 +0000

    cleanup and may remove

--- dsg_lib/__init__.py
+++ dsg_lib/__init__.py
@@ -4,12 +4,6 @@
 This module is the entry point for the package. It imports the necessary functions and modules
 from the package submodules so they can be used directly after importing the package.
 """
-
-# Importing patterns from common_functions module
-# Importing logging configuration from common_functions module
-# Importing folder functions from common_functions module
-# Importing file functions from common_functions module
-# Importing calendar functions from common_functions module
 from .common_functions import (
     calendar_functions,
     file_functions,
@@ -18,12 +12,5 @@ from .common_functions import (
     patterns,
 )
 
-# Importing database operations from database module
-# Importing database configuration from database module
-# Importing async database from database module
-from .database import async_database, database_config, database_operations
-
-# Importing system tools endpoints from endpoints module
-# Importing system health endpoints from endpoints module
-# Importing HTTP codes from endpoints module
-from .endpoints import http_codes, system_health_endpoints, system_tools_endpoints
+# from .database import async_database, database_config, database_operations
+# from .fastapi_endpoints import http_codes, system_health_endpoints
