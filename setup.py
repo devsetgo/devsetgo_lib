55cab1d62f0390b861e23df2ef81144e84b5a0f7
devsetgo <mikeryan56@gmail.com>
2023-12-11 00:57:50 +0000

    update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 # This call to setup() does all the work
 setup(
     name="devsetgo_lib",
-    version="0.10.1",
+    version="0.11.0",
     description="Common functions for applications",
     long_description=README,
     long_description_content_type="text/markdown",
