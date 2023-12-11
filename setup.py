158eac5c03f144eac1cecb0ff280f19dba3822cc
Mike <mikeryan56@gmail.com>
2023-12-11 10:57:35 -0500

    Update setup.py
    Working on issue where build did not include sub folders.

--- setup.py
+++ setup.py
@@ -42,7 +42,8 @@ setup(
         "Operating System :: Microsoft :: Windows",
     ],
     python_requires=">=3.9",
-    packages=["dsg_lib"],
+    # packages=["dsg_lib"],
+    packages=find_packages(where='dsg_lib'),
     include_package_data=True,
     install_requires=[
         "loguru>=0.7.0",
