ad4d157e0d08d03308a6cf07e1947a80c8654b7e
devsetgo <mikeryan56@gmail.com>
2025-04-17 00:49:08 +0000

    bump of version 2025.4.5.1

--- dsg_lib/__init__.py
+++ dsg_lib/__init__.py
@@ -8,7 +8,7 @@ License: MIT
 """
 from datetime import date
 
-__version__ = "2025.04.05-001"
+__version__ = "2025.04.16-001"
 __author__ = "Mike Ryan"
 __license__ = "MIT"
 __copyright__ = f"Copyright© 2021-{date.today().year}"
