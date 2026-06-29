bdcb81b804f332fc990af142aa6817dc9950d114
devsetgo <mikeryan56@gmail.com>
2026-06-29 17:04:42 +0000

    bump of version 2026-06-29-002

--- .bumpcalver/backups/20260629_170417_716___init__.py
+++ .bumpcalver/backups/20260629_170417_716___init__.py
@@ -8,7 +8,7 @@ License: MIT
 """
 from datetime import date
 
-__version__ = "2026.6.29.2"
+__version__ = "2026.6.29.1"
 __author__ = "Mike Ryan"
 __license__ = "MIT"
 __copyright__ = f"Copyright© 2021-{date.today().year}"
